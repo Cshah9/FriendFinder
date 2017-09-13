0b2ffe122035902dacb99b8f248b058341140b80
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-13 04:35:03 -0400

    created package.json and initial server.js

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
-var friends = require('./app/data/friends.js');
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
 
-console.log(friends[0]);
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
+
+// Routes =============================================================
+
+// require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+
+
+app.listen(PORT, function() {
+console.log("App listening on PORT " + PORT);
+});
+
