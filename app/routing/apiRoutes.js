83e11e1dfe02dbc5817dc27b0a4b9848ec4af98f
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-13 06:19:28 -0400

    compatibale calc in post api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,10 @@
+var friends = require("../data/friends.js");
+
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
    console.log("GET api/friends");
+   res.json(friends);
+
   });
 
  
@@ -9,6 +13,22 @@ module.exports = function(app) {
     
     console.log("POST api/friends");
     console.log("req.body", req.body);
+
+    //compare to friends
+    var totals = [];
+
+    for (var i = 0; i < friends.length; i++) {
+      
+         totals[i] = 0; 
+      for (var j = 0; j < friends[i].scores.length; j++) {
+          totals[i] += Math.abs(req.body.q[j] - friends[i].scores[j]);
+       } 
+
+       // friends[i]
+    }
+    console.log("totals", totals);
+
+    if (totals[0]<=totals[1]) {res.json({name:friends[0].name, photo:friends[1].photo})};
 
   });
 
